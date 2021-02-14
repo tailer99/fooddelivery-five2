77f1293eb512917ed7bb3ab9563a7b0cb3f30f60
Danial Pyo <tailer9999@gmail.com>
2021-02-14 11:07:41 +0900

    modify ERRORs2

--- order/src/main/java/fooddeliveryfive/MenuDeleted.java
+++ order/src/main/java/fooddeliveryfive/MenuDeleted.java
@@ -6,11 +6,11 @@ public class MenuDeleted extends AbstractEvent {
     private Long menuId;
     private String menuName;
 
-    public Long getId() {
+    public Long getMenuId() {
         return menuId;
     }
 
-    public void setId(Long menuId) {
+    public void setMenuId(Long menuId) {
         this.menuId = menuId;
     }
     public String getMenuName() {
